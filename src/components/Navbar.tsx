e9c84d72534a04548e0ec79aa99f91871a16b75f
deep04shikha11 <deepshikhaverma44@gmail.com>
2025-08-25 16:25:05 +0530

    add hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <nav className="bg-white shadow-md sticky top-0 z-50" >
             <div className="max-w-6l mx-auto px-4 py-3 flex justify-between items-center">
-                <Link href="/" className="text-xl font-bold text-gray-800"> MyPortfolio </Link>
+                <Link href="/" className="text-xl font-bold text-gray-800"> My Portfolio </Link>
             </div>
         </nav>
     );
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,19 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
 export default function Hero() {
   return (
-    <section className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to My Portfolio 🚀</h1>
+    <section className="h-screen flex flex-col justify-center items-center text-center bg-gray-100">
+      <motion.h1 className="text-5xl font-bold" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }}>
+         👋 Hi, I’m <span className="text-blue-500">DeepShikha Verma</span>
+      </motion.h1>
+      <motion.p className="mt-4 text-xl text-gray-600" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }}>
+        Full stack web developer | Building web experiences        
+      </motion.p>
+      <motion.div className="mt-6" initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1}}>
+        <Link href="./resume.pdf" target="_blank" className="px-6 py-3 bg-blue-500 text-white rounded-xl shadow hover:bg-blue-600">
+          Download Resume
+        </Link>
+      </motion.div>
     </section>
   );
 }

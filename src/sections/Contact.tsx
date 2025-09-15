c11a417c4ff5175e3f9a9a42d914365b308bd5b1
deep04shikha11 <deepshikhaverma44@gmail.com>
2025-09-15 13:22:56 +0530

    add social media links

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Contact() {
     const [status, setStatus] = useState<string>("");
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -28,6 +29,15 @@ export default function Contact() {
                 <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Send</button>
                 <p>{status}</p>
             </form>
+            <div className="mt-8 flex justify-center gap-6 text-3xl">
+                <a href="https://github.com/deep04shikha11" target="_blank" rel="noopener noreferrer" className="hover:text-gray-800">
+                <FaGithub/>
+                </a>
+                <a href="https://www.linkedin.com/in/deepshikha-verma-66636910b/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-700">
+                <FaLinkedin/>
+                </a>
+            </div> 
+
         </section>
     );
 }
